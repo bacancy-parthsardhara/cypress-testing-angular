5bb7aef1b4421363ad312e6acd98934667428d09
bacancy-parthsardhara <parth.sardhara@bacancy.com>
2022-04-01 12:52:42 +0530

    Added 2 basic cypress test for the login page demo.

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,22 @@
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
+  it('Should visit the login page', () => {
+    cy.visit('/login');
+    cy.url().should('includes', 'login');
+    cy.get('#loginFormTitle').should('be.visible');
+    cy.get('#loginFormTitle').should('have.text', 'Login Form');
+    cy.get('#loginFormEmailInputValue').should('not.exist');
+    cy.get('#loginFormPasswordInputValue').should('not.exist');
+  })
+
+  it('Should enter valid email and password and redirect to the dashboard', () => {
+    cy.visit('/login');
+    cy.url().should('includes', 'login');
+    cy.get('#loginFormEmailInput').type('parth@gmail.com');
+    cy.get('#loginFormPasswordInput').type('Parth@123');
+    cy.get('#loginFormSubmitButton').click();
+    cy.get('#loginFormEmailInputValue').should('be.visible');
+    cy.get('#loginFormEmailInputValue').should('have.text', 'Email: parth@gmail.com');
+    cy.get('#loginFormPasswordInputValue').should('be.visible');
+    cy.get('#loginFormPasswordInputValue').should('have.text', 'Password: Parth@123');
   })
 })
